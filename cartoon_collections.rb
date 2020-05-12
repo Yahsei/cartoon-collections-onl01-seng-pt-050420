e6d4ca71324229dcaee11cac964182c78fe2f748
Jesse Summer-Lavigne <jessesl22@gmail.com>
2020-05-12 18:41:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
 def roll_call_dwarves(array)
   i = 0
   while i < array.length 
-  puts "#{i + 1} #{array[i])""
+  puts "#{i + 1} #{array[i])"
  
   i += 1
   end
