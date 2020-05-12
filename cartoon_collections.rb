ea2993bac61508ca6c3ccb998c2e9f3e6b52ae53
Jesse Summer-Lavigne <jessesl22@gmail.com>
2020-05-12 19:07:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,6 +23,7 @@ def long_planeteer_calls(array)
     call.length > 4
 end
 end
+
 def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
