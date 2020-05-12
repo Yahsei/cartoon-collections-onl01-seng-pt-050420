29daa953672a6a22a0c7d69dabe6816a6ba73e68
Jesse Summer-Lavigne <jessesl22@gmail.com>
2020-05-12 18:45:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ end
 
 planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)
   # Your code here
 end
 
